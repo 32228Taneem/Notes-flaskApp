721106435405bbe2a4d9610e009deb7bb70b52b2
32228Taneem <2000032228@kluniversity.in>
2024-12-26 11:53:52 +0530

    css uplodefile ka

--- otp.py
+++ otp.py
@@ -3,7 +3,7 @@ def genotp():
     otp=''
     for i in range(2):
         otp=otp+random.choice([chr(i) for i in range(ord('A'),ord('Z')+1)])
-        otp=otp+random.choice([chr(i) for i in range(ord('A'),ord('Z')+1)])
+        otp=otp+random.choice([chr(i) for i in range(ord('a'),ord('z')+1)])
         otp=otp+str(random.randint(0,9))
     print(otp)
     return otp
